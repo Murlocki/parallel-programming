74763371cf2c692ba93e80861abbf464536cfdb8
Murlocki <kirillgod6@gmail.com>
2024-03-06 01:08:09 +0300

    Complete lab3

--- lab3/Jacobi/Jacobi.cpp
+++ lab3/Jacobi/Jacobi.cpp
@@ -18,12 +18,11 @@ double Simpson(double a, double b, double N) {
     double res = 0;
     for (int i = 0; i < N-1; i=i+2) {
         res += step / 3 * (func_to_calc(X_int[i]) + 4 * func_to_calc((double)(X_int[i + 1])) + func_to_calc(X_int[i + 2]));
-        cout << func_to_calc(X_int[i]) << " " << 4 * func_to_calc((double)(X_int[i + 1])) << " " << func_to_calc(X_int[i + 2]) << endl;
     }
     return res;
 }
 
-double Simpson_parallel(double a, double b, int N) {
+double Simpson_parallel(double a, double b, int N,int num_thr) {
     double step = (double)(b - a) / N;
     double* X_int = new double[N + 1];
     X_int[0] = a;
@@ -31,18 +30,38 @@ double Simpson_parallel(double a, double b, int N) {
         X_int[i] = X_int[i - 1] + step;
     }
     double res = 0;
-    #pragma omp parallel num_threads(3)
+    #pragma omp parallel num_threads(num_thr) reduction(+:res)
         {
-            #pragma omp for 
+            #pragma omp for collapse(1)
             for (int i = 0; i < N - 1; i = i + 2) 
             {
                 res += step / 3 * (func_to_calc(X_int[i]) + 4 * func_to_calc((double)(X_int[i + 1])) + func_to_calc(X_int[i + 2]));
-                cout << func_to_calc(X_int[i]) << " " << 4 * func_to_calc((double)(X_int[i + 1])) << " " << func_to_calc(X_int[i + 2]) << endl;
             }
         };
     return res;
 }
-
+double Simpson_parallel_2(double a, double b, int N,int num_thr) {
+    double step = (double)(b - a) / N;
+    double* X_int = new double[N + 1];
+    X_int[0] = a;
+    for (int i = 1; i < N + 1; i++) {
+        X_int[i] = X_int[i - 1] + step;
+    }
+    double* res_f = new double[3];
+    double res = 0;
+    #pragma omp parallel num_threads(num_thr) reduction(+:res)
+    {
+        #pragma omp for collapse(2) 
+        for (int i = 0; i < N - 1; i = i + 2)
+        {
+            for (int j = 0; j < 3; j++) {
+                res_f[j] = func_to_calc(X_int[i + j]);
+            }
+            res += step / 3 * (res_f[0] + 4 * res_f[1] + res_f[2]);
+        }
+    };
+    return res;
+}
 int main()
 {
     setlocale(LC_ALL, "russian");
@@ -54,8 +73,37 @@ int main()
     cin >> b;
     cout << "Количество отрезков для разбиения" << endl;
     cin >> N;
-    cout << Simpson_parallel(a, b, N);
-    cout << Simpson(a, b, N);
+
+    double start_time = clock();
+    cout << Simpson_parallel_2(a, b, N, 2) << endl;
+    double end_time = clock();
+    cout <<"Второй вид 2 ядра:"<<end_time - start_time << endl;
+    start_time = clock();
+    cout << Simpson_parallel(a, b, N, 2) << endl;
+    end_time = clock();
+    cout << "Первый вид 2 ядра:" << end_time - start_time << endl;
+
+    start_time = clock();
+    cout << Simpson_parallel_2(a, b, N, 3) << endl;
+    end_time = clock();
+    cout << "Второй вид 3 ядра:" << end_time - start_time << endl;
+    start_time = clock();
+    cout << Simpson_parallel(a, b, N, 3) << endl;
+    end_time = clock();
+    cout << "Первый вид 3 ядра:" << end_time - start_time << endl;
+
+    start_time = clock();
+    cout << Simpson_parallel_2(a, b, N,4)<<endl;
+    end_time = clock();
+    cout << "Второй вид 4 ядра:" << end_time - start_time << endl;
+    start_time = clock();
+    cout << Simpson_parallel(a, b, N,4)<<endl;
+    end_time = clock();
+    cout << "Первый вид 4 ядра:" << end_time - start_time << endl;
+    start_time = clock();
+    cout << Simpson(a, b, N) << endl;
+    end_time = clock();
+    cout << "Последовательный вид:" << end_time - start_time << endl;
 }
 
 
